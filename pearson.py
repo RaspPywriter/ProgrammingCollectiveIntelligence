81382d57e724d8467552e6cdb290c7322da060a1
RaspPywriter <39004382+RaspPywriter@users.noreply.github.com>
2019-03-10 17:35:42 -0400

    Update pearson.py

--- pearson.py
+++ pearson.py
@@ -1,4 +1,4 @@
-crcritics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
+critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
  'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5,
  'The Night Listener': 3.0},
 'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5,
